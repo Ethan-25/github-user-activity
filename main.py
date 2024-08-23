acdc409a858b0bb1cec1b538c5c7ba388d1ec37e
Ethan-25 <ethan1986616@gmail.com>
2024-08-23 20:11:33 +0800

    modify the Func parse_activity&display_activities

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
 import requests
+from colorama import init, Fore
+
+init(autoreset=True)
 
 
 def fetch_github_activity(username):
@@ -10,7 +13,7 @@ def fetch_github_activity(username):
         response = requests.get(url, headers=headers)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
+        print(Fore.RED + f"Error fetching data: {e}")
         sys.exit(1)
 
     return response.json()
@@ -21,10 +24,17 @@ def parse_activity(events):
     for event in events:
         event_type = event.get("type")
         repo_name = event.get("repo", {}).get("name")
+        message = ""
+
+        if event_type == 'PushEvent':
+            # Safely extract the commit message
+            #
+            commits = event.get("payload", {}).get("commits", [])
+            if commits:
+                message = commits[0].get("message", "")
 
         if event_type and repo_name:
-            activity = f"{event_type} in {repo_name}"
-            activities.append(activity)
+            activities.append((event_type, repo_name, message))
 
     return activities
 
@@ -33,13 +43,16 @@ def display_activities(activities):
     if not activities:
         print("No recent activity found.")
     else:
-        for activity in activities:
-            print(f"- {activity}")
+        for event_type, repo_name, message in activities:
+            if event_type == 'PushEvent':
+                print(Fore.RED + f"{event_type}", "in", Fore.BLUE + f"{repo_name} - ", Fore.GREEN + f"{message}")
+            else:
+                print(Fore.RED + f"{event_type}", "in", Fore.BLUE + f"{repo_name} - ")
 
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python github_activity.py <github_username>")
+        print(Fore.RED + "Usage: python github_activity.py <github_username>")
         sys.exit(1)
 
     username = sys.argv[1]
